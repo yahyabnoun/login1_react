27b92c90b1b33863dde2529f29efbdc093496dcb
yahya bnoun <yahyaiko22@gmail.com>
2022-10-26 11:00:14 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+      <div className=" App">
+        <div>
+          <p>LOGIN</p>
+        </div>
+        <div>
+          <label>User Name </label>
+          <input type={"text"}></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input type={"text"}></input>
+        </div>
+        <button>LOGIN</button>
+      </div>
+  );
+}
+
+export default App;
