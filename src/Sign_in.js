2ebfbf8dd5f77bc87394984b97b9c638ce7755cc
yahya bnoun <yahyaiko22@gmail.com>
2022-10-27 20:57:37 +0100

    initial commit

--- src/Sign_in.js
+++ src/Sign_in.js
@@ -3,11 +3,29 @@ import {Link} from "react-router-dom";
 import Validation from './Validation'
 
 
+
+function saveLocalTodos(Account) {
+  //CHECK --- HEY DO I already have thing in there?
+  let Accounts
+  if (localStorage.getItem('Accounts')===null) {
+    Accounts=[]
+      
+  }
+  else{
+    Accounts=JSON.parse(localStorage.getItem('Accounts'))
+  }
+  Accounts.push(Account)
+  localStorage.setItem('Accounts',JSON.stringify(Accounts))
+}
+///////////////////////////////////////////////////////
 function Sign_in_valid() {
 
 if (Validation()) {
   
-  alert('valide')
+  saveLocalTodos({
+    name:document.querySelectorAll('input')[0].value,
+    passeword:document.querySelectorAll('input')[1].value
+  })
 }else{
   alert('invalide')
 }
