1df09d1f55b041125d7f5f1d3157302c34ecc543
yahya bnoun <yahyaiko22@gmail.com>
2022-10-27 11:26:32 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
-
+import Login from "./Login";
 function App() {
   return (
       <div className=" App">
-        <div>
-          <p>LOGIN</p>
-        </div>
-        <div>
-          <label>User Name </label>
-          <input type={"text"}></input>
-        </div>
-        <div>
-          <label>Password</label>
-          <input type={"text"}></input>
-        </div>
-        <button>LOGIN</button>
+        <Login/>
       </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Login() {
+    return (
+        <div className="main">
+          <div>
+            <h2>LOGIN</h2>
+          </div>
+          <div>
+            <label>User Name </label>
+            <input type={"text"}></input>
+          </div>
+          <div>
+            <label>Password</label>
+            <input type={"text"}></input>
+          </div>
+          <button>LOGIN</button>
+        </div>
+    );
+  }
+  
+  export default Login;
